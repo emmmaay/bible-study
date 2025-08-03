d93391b60e98c2d7a067d67eeb91036fc4197f3c
emmmaay <danvvbb84@gmail.com>
2025-08-03 10:25:23 +0100

    Create login.tsx

---
+++ client/src/pages/login.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-primary-700 to-primary-900">
+      <h1 className="text-white">Login Page</h1>
+    </div>
+  );
+};
+
+export default Login;
